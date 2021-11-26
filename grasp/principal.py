1cc39e7204b9b45690e3445d1326f18634e1f24f
marcus-terra <95049599+marcus-terra@users.noreply.github.com>
2021-11-25 23:40:41 -0400

    Update principal.py

--- grasp/principal.py
+++ grasp/principal.py
@@ -4,20 +4,16 @@ import numpy  as np
 import pandas as pd
 import fitness
 import grasp
+import constantes as const
 
 ### GERA UMA SOLUCAO ALEATORIA [GRADE DE HORARIOS,FITNESS DA SOLUCAO] 
 ### COM BASE NO CONJUNTO DE AULAS NECESSARIAS 
 ### (DISCIPLINA, PROFESSOR)
 
 def solucao_aleatoria(aulas):
-    solucao = [[],float("inf")]
-    # Solucao alternativa que gerar uma sequencia aleatorias de 50 numeros 
-    # entre 0 e 49 que representam a ordem das aulas
-    #sequencia = random.sample(list(range(0,len(aulas))), len(aulas))
-    #solucao[0] = sequencia
-    #solucao[1] = funcao_objetivo(aulas, sequencia)
-    solucao[0] = random.sample(aulas, len(aulas))
-    solucao[1] = fitness.funcao_objetivo(solucao[0])
+    solucao = const.PADRAO_SOLUCAO_INICIAL_VAZIA
+    solucao[const.INDICE_GRADE] = random.sample(aulas, len(aulas))
+    solucao[const.INDICE_FITNESS] = fitness.funcao_objetivo(solucao[const.INDICE_GRADE])
     return solucao
 
 ### FUNCAO QUE TRANSFORMA A ESTRUTURA DO TIPO
@@ -34,40 +30,51 @@ def gera_aulas(prof_disc_hora):
     aulas = [] 
     for i in range(0, prof_disc_hora.shape[0]):
         aula = prof_disc_hora[i] 
-        qtde_horarios_aula = aula[2]
+        qtde_horarios_aula = aula[const.INDICE_HORARIOS]
         for j in range(0, qtde_horarios_aula):
-          aulas.append((aula[0],aula[1]))
+          aulas.append((aula[const.INDICE_PROFESSOR],aula[const.INDICE_DISCIPLINA]))
     # Se a quantidade de aulas < 50 preenche a lista aulas com valores VAGO
-    for i in range(len(aulas),50):
-        aulas.append(('VAGO','VAGO'))
+    for i in range(len(aulas),const.TOTAL_HORARIOS_GRADE):
+        aulas.append((const.HORARIO_VAGO, const.HORARIO_VAGO))
     return aulas #np.array(aulas,dt)
 
 ### FUNCAO QUE IMPRIME UMA SOLUCAO [GRADE DE HORARIOS, FITNESS DA SOLUCAO]
 ### DE FORMA AMIGÀVEL
 
 def imprime_solucao(solucao):
-    dias = ['Segunda','Terça','Quarta','Quinta','Sexta']
-    for i in range(0,len(solucao[0])):
-        dia = dias[i//10]
-        if (i % 10 == 0):
+    for i in range(0,len(solucao[const.INDICE_GRADE])):
+        dia = DIAS_DA_SEMANA[i // const.TOTAL_AULAS_DIA]
+        if (i % const.TOTAL_AULAS_DIA == 0):
             print('\n\n',dia)
-        if (i % 5 == 0):
-            print('\nHorario ', 1+ i % 2, '--------------------')
-        print(solucao[0][i], end = ' | ')
-    print('\nFitness da Solucao = ',solucao[1])    
+        if (i % const.TOTAL_SALAS == 0):
+            print('\nHorario ', 1 + i % 2, '--------------------')
+        # imprime um conjunto [PROFESSOR,DISCIPLINA]    
+        print(solucao[const.INDICE_GRADE][i], end = ' | ')
+    print('\nFitness da Solucao = ',solucao[const.INDICE_FITNESS])    
     return
 
 ### METODO PRINCIPAL QUE RECEBE A URL DO CASO DE TESTE E RETORNA 
 ### A MELHOR SOLUCAO [GRADE DE HORARIOS, FITNESS DA SOLUCAO] ENCONTRADA
 ### 
-def principal(url_caso_de_teste, max_iteracoes = 1000, calcula_solucao_inicial = True):
-    prof_disc_hora = pd.read_csv(url_caso_de_teste, sep = ';')
+def principal(url_caso_de_teste, 
+              calcula_solucao_inicial = const.PADRAO_FLAG_CALCULA_SOLUCAO_INICIAL,
+              max_iteracoes = const.PADRAO_MAX_ITERACOES_GRASP, 
+              alfa_rcl = const.PADRAO_ALFA_RCL, 
+              limite = const.PADRAO_LIMITE_GRASP,
+              max_iteracoes_busca_local = const.PADRAO_MAX_ITERACOES_BUSCA_LOCAL
+              limite_busca_local = const.PADRAO_LIMITE_BUSCA_LOCAL):
+    prof_disc_hora = pd.read_csv(url_caso_de_teste, sep = const.SEPARADOR_CSV)
     prof_disc_hora = prof_disc_hora.values
     aulas = gera_aulas(prof_disc_hora)
+    solucao_inicial = const.PADRAO_SOLUCAO_INICIAL_VAZIA
     if (calcula_solucao_inicial):
         solucao_inicial = solucao_aleatoria(aulas)      
-        melhor_solucao = grasp.grasp_grade(aulas, solucao_inicial, max_iteracoes)
-    else:
-        melhor_solucao = grasp.grasp_grade(aulas, max_iteracoes = max_iteracoes)
+    melhor_solucao = grasp.grasp_grade(aulas, 
+                                       solucao_inicial = solucao_inicial, 
+                                       max_iteracoes = max_iteracoes,                        
+                                       alfa_rcl = alfa_rcl, 
+                                       limite = limite,
+                                       max_iteracoes_busca_local = max_iteracoes_busca_local
+                                       limite_busca_local = limite_busca_local)
     imprime_solucao(melhor_solucao)
     return
