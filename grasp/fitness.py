49136ff72c25b3e352730123153ea7b414fc6f65
marcus-terra <95049599+marcus-terra@users.noreply.github.com>
2021-11-25 23:18:44 -0400

    Update fitness.py

--- grasp/fitness.py
+++ grasp/fitness.py
@@ -1,9 +1,10 @@
 import copy
+import constantes as cont
 
 ### FUNCAO OBJETIVO (FITNESS)
 ### AS FORMAS DO CALCULO ESTAO DEFINIDAS NOS COMENTARIOS DENTRO DA FUNCAO
 ### É UMA FUNCAO DE MINIMIZACAO QUE RETORNA VALOR ENTRE 0 E 1 
-def funcao_objetivo(solucao):
+def funcao_objetivo(grade):
     
     violacoes = 0;
 # A mesma sala não poderá ter mais de uma aula ao mesmo tempo;
@@ -13,20 +14,18 @@ def funcao_objetivo(solucao):
 # 2) Um professor não poderá dar 2 ou mais aulas ao mesmo tempo;
 # A premissa 2) já engloba a premissa 1) pois cada disciplina
 # tem sempre o mesmo professor
-    total_dias = 5
-    total_horarios = 2
-    total_salas = 5
-    for dia in range(total_dias):
-        for horario in range(total_horarios):
-            for sala_atual in range(total_salas):
-                evento_atual = dia*total_dias*total_horarios+horario*total_salas+sala_atual 
-                professor_atual = solucao[evento_atual][0]
-                if (professor_atual != 'VAGO'):
-                    for proxima_sala in range(sala_atual+1,total_salas):
-                        proximo_evento = dia*total_dias*total_horarios+horario*total_salas+proxima_sala
-                        proximo_professor = solucao[proximo_evento][0]
+    
+    for dia in range(const.TOTAL_DIAS):
+        for horario in range(const.TOTAL_HORARIOS_DIA):
+            for sala_atual in range(const.TOTAL_SALAS):
+                evento_atual = dia*const.TOTAL_DIAS*const.TOTAL_HORARIOS_DIA+horario*const.TOTAL_SALAS+sala_atual 
+                professor_atual = grade[evento_atual][const.INDICE_PROFESSOR]
+                if (professor_atual != const.HORARIO_VAGO):
+                    for proxima_sala in range(sala_atual+1,const.TOTAL_SALAS):
+                        proximo_evento = dia*const.TOTAL_DIAS*const.TOTAL_HORARIOS_DIA+horario*const.TOTAL_SALAS+proxima_sala
+                        proximo_professor = grade[proximo_evento][const.INDICE_PROFESSOR]
                         if (professor_atual == proximo_professor):
-                            violacoes+=1
+                            violacoes += const.PESO_PROF_MESMO_HORARIO
                             # evita a recontagem quando o mesmo professor 
                             # aparece mais de 2 vezes no mesmo dia e horario
                             break
@@ -36,19 +35,19 @@ def funcao_objetivo(solucao):
 # é desejável que as aulas não sejam na sequência imediata (mesmo dia);
 # Como é uma premissa desejável e não obrigatória o peso da violacao é 0.1
     
-    for dia in range(total_dias):
-        for sala_h1 in range(total_salas):
-            evento_h1 = dia*total_dias*total_horarios+sala_h1
-            disciplina_h1 = solucao[evento_h1][1]
-            if (disciplina_h1 != 'VAGO'):
-                for sala_h2 in range(total_salas):
-                    evento_h2 = dia*total_dias*total_horarios+sala_h2+total_salas
-                    disciplina_h2 = solucao[evento_h2][1]
+    for dia in range(const.TOTAL_DIAS):
+        for sala_h1 in range(const.TOTAL_SALAS):
+            evento_h1 = dia*const.TOTAL_DIAS*const.TOTAL_HORARIOS_DIA+sala_h1
+            disciplina_h1 = grade[evento_h1][const.INDICE_DISCIPLINA]
+            if (disciplina_h1 != const.HORARIO_VAGO):
+                for sala_h2 in range(const.TOTAL_SALAS):
+                    evento_h2 = dia*const.TOTAL_DIAS*const.TOTAL_HORARIOS_DIA+sala_h2+const.TOTAL_SALAS
+                    disciplina_h2 = grade[evento_h2][const.INDICE_DISCIPLINA]
                     if (disciplina_h1 == disciplina_h2): # a mesma disciplina acontece no mesmo dia
                         if (sala_h1 == sala_h2): # se for na mesma sala a penalizacao reduz pela metade
-                            violacoes+=0.05
+                            violacoes+=const.PESO_DISC_MESMO_DIA_SALA_IGUAL
                         else:
-                            violacoes+=0.1
+                            violacoes+=const.PESO_DISC_MESMO_DIA_SALA_DIFER
                         # a regra anterior já penaliza a mesma disciplina no mesmo horário
                         # assim para não penalizar novamente o laço é interrompido
                         break 
@@ -57,41 +56,27 @@ def funcao_objetivo(solucao):
 # É desejável que não existam buracos (horários/salas sem aula)
 # na grade de horário
 # Considera-se o número total de posicoes da grade (salas*dias*horarios) = 50 
-# Como é uma premissa desejável e não obrigatória o peso da violacao é 0.1
+# Como é uma premissa desejável e não obrigatória o peso da violacao é:
+# 0.1 Se o horário vago for o primeiro do dia
+# 0.05 Se o horario vago for o segundo do dia
     
-    posicao_final = 49;
-    while (posicao_final > -1 and solucao[posicao_final][1] == 'VAGO'):
-        posicao_final-=1
+    posicao_final = const.TOTAL_HORARIOS_GRADE - 1;
+    while (posicao_final > -1 and grade[posicao_final][const.INDICE_PROFESSOR] == const.HORARIO_VAGO):
+        posicao_final -= 1
     for i in range(0, posicao_final+1):
-        if (solucao[i][0] == 'VAGO'):
-            if ((i // 5) % 2 == 0): # o horario sem aula é o primeiro do dia
-               violacoes+=0.1
+        if (grade[i][const.INDICE_PROFESSOR] == const.HORARIO_VAGO):
+            if ((i // const.TOTAL_SALAS) % 2 == 0): # o horario sem aula é o primeiro do dia
+               violacoes+=const.PESO_PRIMEIRO_HORARIO_VAGO
             else:
-               violacoes+=0.05 # o horario sem aula é o segundo do dia (penalização menor)
+               violacoes+=const.PESO_SEGUNDO_HORARIO_VAGO # o horario sem aula é o segundo do dia (penalização menor)
 
-# Solucao alternativa para as salas vagas (buracos na grade)
-# Como não foi identificado problema de performance
-# foi mantida a outra alternativa
-
-    
-#    ultimo_horario_ocupado = -1
-#    salas_ocupadas = 0;
-#    for i in range(0, len(solucao)):
-#        if (solucao[i][0] != 'VAGO'):
-#            ultimo_horario_ocupado = i
-#            salas_ocupadas +=1
-     # total de salas até o ultimo horario ocupado
-#    total_de_salas = ultimo_horario_ocupado + 1
-#    salas_vagas = total_de_salas - salas_ocupadas
-#    violacoes = violacoes + 0.1*salas_vagas
- 
 
 # OBS: Uma questao importante é saber se o peso da violacao 
 # de deixar um horario vago deve ser o mesmo que 
-# colocar a mesma disciplina em sequencia
+# colocar a mesma disciplina em sequencia no dia
 
 # O fitness (funcao objetivo) será :
 # Melhor - quanto mais se aproximar de 0 (menos violacoes)
 # Pior - quanto mais se aproximar de 1 (mais violacoes)
-    custo = 1 - 1 / (1 * violacoes + 1);
-    return custo
+    fitness = 1 - 1 / (1 * violacoes + 1);
+    return fitness
