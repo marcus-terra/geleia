4f24c33e6d22ea83067db42c81969254aa5d230b
marcus-terra <95049599+marcus-terra@users.noreply.github.com>
2021-11-29 23:58:15 -0400

    Update aco.py

--- aco/aco.py
+++ aco/aco.py
@@ -1,6 +1,7 @@
 import copy
 import random
-import numpy.random as npr
+import numpy as np
+
 
 # probs = probalidades de seguir cada um dos caminhos
 
@@ -115,21 +116,27 @@ def calcula_eta(aulas, posicoes_grade, caminhos_possiveis):
         eta[i] = 1 - funcao_objetivo(aulas,grade_possivel) # heuristica/fitness (aula, grade_possivel)
     return eta
 
-def calcula_probabilidades(tau, eta, alfa, beta, caminhos_possiveis): 
-    return
+def calcula_probabilidades(tau_i, eta, alfa, beta, caminhos_possiveis): 
+    probabilidade = [0]*len(caminho_possiveis)
+    somatorio = 0
+    for (l in len(caminhos_possiveis))
+        somatorio = somatorio + ((tau_i[caminhos_possiveis[l]]**alfa) * (eta[l]**beta))
+    for (j in len(caminhos_possiveis))
+        probabilidade[j] = (tau_i[caminhos_possiveis[j]]**alfa * eta[j]**beta)/somatorio
+    return probabilidade
 
 def aco_construcao(aulas, tau, alfa, beta):
     solucao = [[],float("inf")]
     posicoes_grade = []
-    caminhos_possiveis = #vetor de 0 a 49
+    caminhos_possiveis = list(range(50)) #vetor de 0 a 49
     posicao_inicial = random.randrange(len(aulas))
     posicoes_grade.append(posicao_inicial)
     caminhos_possiveis.remove(posicao_inicial)
     posicao_atual = posicao_inicial
     while (caminho_possiveis > 0):
         eta = calcula_eta(aulas, grade, caminhos_possiveis)
-        probabilidades = calcula_probabilidades(tau[posicao_atual], eta, alfa, beta, caminhos_possiveis) # o tau sera somente da linha da posicao_atual
-        proxima_posicao = npr.choice(len(probabilidades), p=probabilidades)] # roleta
+        probabilidades = calcula_probabilidades(tau[posicao_atual,:], eta, alfa, beta, caminhos_possiveis) # o tau sera somente da linha da posicao_atual
+        proxima_posicao = np.random.choice(len(probabilidades), p=probabilidades)] # roleta
         posicoes_grade.append(proxima_posicao)
         caminhos_possiveis.remove(proxima_posicao)
     solucao[0] = posicoes_grade
@@ -137,10 +144,19 @@ def aco_construcao(aulas, tau, alfa, beta):
     return solucao
 
 def deposita_feromonio(Q,delta_tau,melhor_solucao):
-    return
+    delta_tau_novo = [[0 for col in range(50)] for lin in range(50)]
+    for (c in range(0,len(melhor_solucao[0])-1)):
+        i = melhor_solucao[0][c]
+        j = melhor_solucao[0][c+1]
+        delta_tau_novo[i][j] = delta_tau[i][j] + Q/melhor_solucao[1]
+    return delta_tau_novo
 
 def atualiza_feromonios(tau, ro, delta_tau):
-    return
+    tau_novo = copy.deepcopy(tau)
+    for (i in range(50)):
+        for (j in range(50)):
+            tau_novo[i][j] = ro*tau[i][j] + delta_tau[i][j]
+    return tau_novo
 
 
 # Q = quantidade de feromônio excretada por uma formiga a cada iteração (Padrão = 1)
@@ -159,15 +175,16 @@ def aco_grade(aulas,
               qtde_formigas = 50):
     contador = 0
     melhor_solucao = solucao_inicial
-    delta_tau = # tamanho 50 x 50
-    tau = # tamanho 50 x 50
+    delta_tau = [[0 for col in range(50)] for lin in range(50)]
+    tau = [[tau_0 for col in range(50)] for lin in range(50)]
     while (contador < max_iteracoes and melhor_solucao[1]>limite):
         for (i in range(qtde_formigas)):
             solucao = aco_construcao(aulas, tau, alfa, beta)
             if (solucao[1] < melhor_solucao[1]):
                 melhor_solucao = solucao
-            delta_tau = deposita_feromonio(Q, delta_tau, melhor_solucao)
-        tau = atualiza_feronomios(tau, ro, delta_tau)
+            # elitista, reforça sempre a melhor solucao, mas nao sei se deposita sempre ou só quando encontra uma nova melhor solução     
+            delta_tau = deposita_feromonio(Q, delta_tau, melhor_solucao) 
+        tau = atualiza_feromonios(tau, ro, delta_tau)
         contador += 1
         print('Iteracao =', contador, '-> Custo Solucao =', melhor_solucao[1])
     return melhor_solucao
